277943fb5e75cb7bf844c0eeacd7e6a30e6a56e5
Anton Persson <antpe404@student.liu.se>
2016-12-05 13:59:56 +0100

    Tree Adaboost

--- lab2bl2_732A95.R
+++ lab2bl2_732A95.R
@@ -8,7 +8,7 @@ library(tree)
 set.seed(1234567890)
 #sampling<-sample(1:nrow(bodyfat))
 bodyfat_samplad<-bodyfat[sample(1:nrow(bodyfat)),]
-bodyfat_tr<-bodyfat_samplad[1:73,] #Träningsset
+bodyfat_tr<-bodyfat_samplad[1:73,] #Traningsset
 bodyfat_te<-bodyfat_samplad[74:110,] #testset
 
 
@@ -30,7 +30,7 @@ lowerbound<-mean((felen/length(felen))**2)
 #"At least, the bagged error is never larger than the average individual errors:"
 #mao vara average felet.
 
-#The upper bound ska istället vara mean av felen:
+#The upper bound ska istallet vara mean av felen:
 
 felen_upper<-integer(0)
 set.seed(1234567890)
@@ -46,22 +46,19 @@ upperbound<-mean((felen_upper/length(felen_upper)))
 #upperbound<-mean(felen_upper)
 
 #2.2----------------------------------------------------------------------------------
-#blir alltså 3 modeller för varje bagging, om jag tänker rätt. Först en modell på fold (1,2), sen (1,3), sen (2,3)
-#börjar med att sampla 
+#blir alltsa 3 modeller for varje bagging, om jag tanker ratt. Forst en modell pa fold (1,2), sen (1,3), sen (2,3)
+#borjar med att sampla 
 #set.seed(1234567890)
-
-
-#set.seed(1234567890)
-#bodyfat_samplad<-bodyfat[sample(1:nrow(bodyfat)),]
-#folds_data<-suppressWarnings(split(bodyfat_samplad, 1:folds)) 
 
 folds<-3
+set.seed(1234567890)
+bodyfat_samplad<-bodyfat[sample(1:nrow(bodyfat)),]
+folds_data<-suppressWarnings(split(bodyfat_samplad, 1:folds)) 
+
 baggingar<-100
 set.seed(1234567890)
 folds_data<-suppressWarnings(split(bodyfat, 1:folds)) 
-#fold1<-data.frame(folds_data$`1`)
-#fold2<-data.frame(folds_data$`2`)
-#fold3<-data.frame(folds_data$`3`)
+
 alla_fel<-matrix(0, nrow=folds, ncol=baggingar)
 
 for (i in 1:folds){
@@ -88,48 +85,105 @@ for (i in 1:folds){
 
 upperbound_2<-mean(alla_fel)
 
+#The upper bound ar mean av alla individuella fel. Rimligt att detta bound ar ngt hogre, eftersom vi kor 3fold CV?
 
 #2.3
 
 
 
 
+#######Assignment 4
+spam<-read.csv2("data/spambaselab2b2.csv", sep=";", header=T)
+spam$Spam<-as.factor(spam$Spam)
+set.seed(1234567890)
+spam_samplad<-spam[sample(1:nrow(spam)), ]
+spam_tr<-spam_samplad[1:round((2/3)*nrow(spam)), ]
+spam_te<-spam_samplad[-(1:round((2/3)*nrow(spam))), ]
+
+#install.packages("mboost")
+library(mboost)
+#install.packages("randomForest")
+library(randomForest)
+library(ggplot2)
 
 
 
-#The upper bound är mean av alla individuella fel. Rimligt att detta bound är ngt högre, eftersom vi kör 3fold CV?
+#Förklaringar till inställningarna:
+#AdaExp() uses the exponential loss, which essentially 
+#leads to the AdaBoost algorithm of Freund and Schapire (1996).
+
+#boost_control(mstop = 100, nu = 0.1,
+#              risk = c("inbag", "oobag", "none"), stopintern = FALSE,
+#              center = TRUE, trace = FALSE)
+#Arguments
+
+#mstop	
+#an integer giving the number of initial boosting iterations.
+
+#--------
+#Examples
+
+
+### a simple two-dimensional example: cars data
+#cars.gb <- blackboost(dist ~ speed, data = cars,
+ #                     control = boost_control(mstop = 50))
+
+modellen<-blackboost(Spam~., data=spam_tr, family=AdaExp(), control=boost_control(mstop=10))
+
+predict(modellen, newdata=spam_te, type="class")
+tejbell_test<-table(pred=predict(modellen, newdata= spam_te, type="class"), truth=spam_te$Spam)
+
+error_rate_test<-1-sum(diag(tejbell_test))/sum(tejbell_test)
+
+tejbell_train<-table(predict(modellen, newdata= spam_tr, type="class"), spam_tr$Spam)
+error_rate_train<-1-sum(diag(tejbell_train))/sum(tejbell_train)
+#Sahar kan man ocksa rakna.
+1 - (sum(predict(modellen, spam_te, type="class") == spam_te$Spam) / nrow(spam_te))
+
+#----------------------------------Adaboost classification tree-------------------------------------------
+sekvens<-seq(10,100, 10)
+training_errors<-integer()
+test_errors<-integer()
+index<-1
+for (i in sekvens){
+  modellen<-blackboost(Spam~., data=spam_tr, family=AdaExp(),  control=boost_control(mstop=i))
+  
+  tejbell_train<-table(predict(modellen, newdata= spam_tr, type="class"), spam_tr$Spam)
+  training_errors[index]<-1-sum(diag(tejbell_train))/sum(tejbell_train)
+  
+  tejbell_test<-table(predict(modellen, newdata= spam_te, type="class"), spam_te$Spam)
+  test_errors[index]<-1-sum(diag(tejbell_test))/sum(tejbell_test)
+  index<-index+1
+}
+
+
+
+
+
+
+for(i in sekvens){
+  assign(paste("error_rate_", i, sep=""), 
+        i+10
+        #Har vill jag ju ha in sjalva funktionen istallet, alltsa det nedan
+        #modellen<-blackboost(Spam~., data=spam_tr, family=AdaExp(),  control=boost_control(mstop=i))
+        #tejbell_test<-table(predict(modellen, newdata= spam_te, type="class"), spam_te$Spam)
+        
+        #1-sum(diag(tejbell_test))/sum(tejbell_test)
+        
+         )
+}
+assign(paste("error_rate", i, sep=""),5)
+
+
+
+
+
 #----
-#set.seed(1234567890)
-#bodyfat_samplad<-bodyfat[sample(1:nrow(bodyfat)),]
-#bodyfat_tr<-bodyfat_samplad[1:73,] #Träningsset
-#bodyfat_te<-bodyfat_samplad[74:110,] #testset
-
-#bf_tr_sam<-bodyfat_tr[sample(1:nrow(bodyfat_tr), replace=T), ]
-
-
-#folds_data<-suppressWarnings(split(bodyfat_samplad, 1:3))
 #fold1<-data.frame(folds_data$`1`)
 #fold2<-data.frame(folds_data$`2`)
 #fold3<-data.frame(folds_data$`3`)
+#set.seed(1234567890)
 
-#set1<-rbind(fold1, fold2)
-#test1<-fold3
-
-#set2<-rbind(fold1, fold3)
-#test2<-fold2
-
-#set3<-rbind(fold2, fold3)
-#test3<-fold1
-
-#tree1<-tree(formula=Bodyfat_percent~., data=set1, split="deviance")
-#fits1<-predict(tree1, newdata=test1)
-#tree2<-tree(formula=Bodyfat_percent~., data=set2, split="deviance")
-#fits2<-predict(tree2, newdata=test2)
-#tree3<-tree(formula=Bodyfat_percent~., data=set3, split="deviance")
-#fits3<-predict(tree3, newdata=test3)
-
-
-#folds<-3
 #komber<-combn(1:folds, folds-1)
 #komber
 #komber[,1]
@@ -137,5 +191,7 @@ upperbound_2<-mean(alla_fel)
 #assign(paste("perf.a", "1", sep=""),5)
 
 #for(i in 1:folds){
-#assign(paste("set", "i", sep=""),rbind(fold[HÄR vill jag ha i komber[,1]],fold2))
+#assign(paste("set", "i", sep=""),rbind(fold[HaR vill jag ha i komber[,1]],fold2))
 #}
+
+#afsaff
