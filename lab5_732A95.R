7e4f9ba77c4624b097b115da1c4056b3f25ba453
Anton Persson <antpe404@student.liu.se>
2016-12-12 14:10:25 +0100

    Runs slow as fuck, but works.

--- lab5_732A95.R
+++ lab5_732A95.R
@@ -14,7 +14,7 @@ h_distance <- 100000 # These three values are up to the students
 a <- 58.4274 # The point to predict (up to the students)
 b <- 14.826
 
-pred_date <- as.Date("2010-12-24") # The date to predict (up to the students)
+pred_date <- as.Date("2016-12-24") # The date to predict (up to the students)
 times <- c("02:00:00","04:00:00", "06:00:00","08:00:00","10:00:00","12:00:00","14:00:00","16:00:00","18:00:00","20:00:00","22:00:00", "24:00:00")
 pred_time<-("12:00:00")
 temp <- vector(length=length(times))
@@ -39,7 +39,7 @@ dist_func<-function(data=data.frame(cbind(longitude=st$longitude, latitude=st$la
   return(distances)
 }
 
-#dist_diff<-dist_func(city_coord = nkpg_longlat)
+dist_diff<-dist_func(city_coord = c(b,a))
   
 ############Days Kernel, to account for number of days between observations and day of interest########
 
@@ -79,7 +79,7 @@ return(hours_differential)
 
 totfunk<-function(data=st, h_time, h_days, h_distance, city_coord, pred_time, pred_date){
   #data<-st
-  #city_coord<-nkpg_longlat
+  #city_coord<-c(b,a)
   hours<-substr((data$time), start=1, stop=8) #Defined to be able to cut the data
   dates<-as.Date(data$date) #Same here
   logic_date_vector<-paste(dates, hours)<paste(pred_date, pred_time)
@@ -108,10 +108,13 @@ totfunk<-function(data=st, h_time, h_days, h_distance, city_coord, pred_time, pr
 totfunk(h_time=3, h_days=4, h_distance=100000, city_coord=nkpg_longlat, pred_time=pred_time,
         pred_date = pred_date)
 
-totfunk(h_time=3, h_days=4, h_distance=100000, city_coord=nkpg_longlat, pred_time="00:00:00",
+totfunk(h_time=3, h_days=4, h_distance=100000, city_coord=nkpg_longlat, pred_time="02:00:00",
         pred_date = pred_date)
 
 totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=c(a,b), pred_time="02:00:00",
+        pred_date = as.Date("2016-12-24"))
+
+totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=c(b,a), pred_time="02:00:00",
         pred_date = as.Date("2016-12-24"))
 
 totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=c(a,b), pred_time="12:00:00",
@@ -119,19 +122,39 @@ totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=c(a,b), pred_time="12:
 
 ##################################Jmf med de andras resultat nedan.###################
 for (i in 1:length(times)){
-  temp[i]<-totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=c(a,b), pred_time=times[i],
+  temp[i]<-totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=c(b,a), pred_time=times[i],
                 pred_date = as.Date("2016-12-24"))
 }
 
 hourly_predictions<-data.frame(cbind(times, temp=round(temp, 5)))
 
+hourly_predictions2<-hourly_predictions
+hourly_predictions2$times<-as.factor(hourly_predictions2$times)
+hourly_predictions2$temp<-as.numeric(as.character(hourly_predictions2$temp))
+hourly_predictions2
+library(ggplot2)
+
+dec16<-ggplot(data=hourly_predictions2)+geom_point(aes(x=times, y=temp))+geom_line(aes(x=times, y = temp))+
+  ggtitle("Temperature predicition Vadstena on christmas day")+ylab(label = "Degrees celcius")
+
+dec16
+
+plot(x=hourly_predictions$times, y=hourly_predictions$temp)
+
 for (i in 1:length(times)){
-  temp[i]<-totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=c(a,b), pred_time=times[i],
+  temp[i]<-totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=c(b,a), pred_time=times[i],
                    pred_date = as.Date("2016-07-24"))
 }
 
 hourly_predictions_summer<-data.frame(cbind(times, temp=round(temp, 5)))
+hourly_predictions_summer2<-hourly_predictions_summer
+hourly_predictions_summer2$times<-as.factor(hourly_predictions_summer$times)
+hourly_predictions_summer2$temp<-as.numeric(as.character(hourly_predictions_summer$temp))
 
+juli16<-ggplot(data=hourly_predictions_summer2)+geom_point(aes(x=times, y=temp))+geom_line(aes(x=times, y = temp))+
+  ggtitle("Temperature prediction Vadstena 2016-07-24")+ylab(label = "Degrees celcius")
+
+juli16
 #Detta beror på att time_ker blir så jävla liten, så den påverkar inte alls. Det enda som räknas in är alltså
 #distance och tid på dygnet, vilket ju är samma för vinter och sommar. Detta eftersom jag predikterar så långt 
 #fram i nuläget, alltså julafton i år. Alla time_ker blir assmå. Jag skulle kunna göra den kerneln cyklisk också,
@@ -146,6 +169,22 @@ for (i in 1:length(times)){
 
 hourly_predictions_multi<-data.frame(cbind(times, temp=round(temp, 5)))
 
+
+
+for (i in 1:length(times)){
+  temp[i]<-totfunk(h_time=2, h_days=7, h_distance=100000, city_coord=nkpg_longlat, pred_time=times[i],
+                   pred_date = as.Date("2000-07-24"))
+}
+
+hourly_predictions_nkpg_summer<-data.frame(cbind(times, temp=round(temp, 5)))
+hourly_predictions_nkpg_summer2<-hourly_predictions_nkpg_summer
+hourly_predictions_nkpg_summer2$times<-as.factor(hourly_predictions_nkpg_summer2$times)
+hourly_predictions_nkpg_summer2$temp<-as.numeric(as.character(hourly_predictions_nkpg_summer2$temp))
+
+juli00_nkpg-ggplot(data=hourly_predictions_nkpg_summer2)+geom_point(aes(x=times, y=temp))+geom_line(aes(x=times, y = temp))+
+  ggtitle("Temperature prediction 2000-07-24")+ylab(label = "Degrees celcius")
+
+juli00_nkpg
 
 ########################### gauss_kernel kladd/test
 #dist_ker<-gauss_kernel(diff=dist_func(city_coord = nkpg_longlat), h=h_distance)
@@ -221,9 +260,17 @@ hourly_predictions_multi<-data.frame(cbind(times, temp=round(temp, 5)))
 #sum(time_ker)
 #[1] 8.561185e-275
 
+#sum(hour_ker)
+#sum(hour_ker+time_ker)
+#sum(hour_ker+time_ker+dist_ker)
+#sums<-list(sum_of_distance_kernel=sum(dist_ker), sum_of_time_kernel=sum(time_ker),
+ #          sum_of_hour_kernel=sum(hour_ker))
+
 #head(dates)<as.Date("2003-01-01")
 #paste(head(dates), head(hours))<paste(pred_date, pred_time)
 #paste(head(dates), head(hours))<paste(as.Date("2000-01-01"), pred_time)
 #När cuttad på julafton 2010: Ser ergo ut att funka.
 #max(as.Date(data$date))
 #[1] "2010-12-23"
+
+#dåligt med obereonde
