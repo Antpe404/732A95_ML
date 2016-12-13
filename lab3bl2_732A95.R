17f16820de8f264a509a48d5ca9ffb376e473738
Anton Persson <antpe404@student.liu.se>
2016-12-13 15:00:26 +0100

    Think I have all it takes for 1.1

--- lab3bl2_732A95.R
+++ lab3bl2_732A95.R
@@ -13,26 +13,25 @@ library(pamr)
 which_col<-which(colnames(train)=="Conference")
 x<-t(train[,-which_col]) #Alla utom Conf
 y<-train[, which_col] #endast Conf
-head(as.character( 1:nrow(x) ))
+#head(as.character( 1:nrow(x) ))
 mydata<-list(x=x,y=y,geneid=as.character(1:nrow(x)), genenames=rownames(x))
 model<-pamr.train(mydata,threshold=seq(0,4, 0.1))
-
-
-
-data0=read.csv2("voice.csv")
-data=data0
-data=as.data.frame(scale(data))
-data$Quality=as.factor(data0$Quality)
-library(pamr)
-rownames(data)=1:nrow(data)
-x=t(data[,-311])
-y=data[[311]]
-mydata=list(x=x,y=as.factor(y),geneid=as.character(1:nrow(x)), genenames=rownames(x))
-model=pamr.train(mydata,threshold=seq(0,4, 0.1))
 pamr.plotcen(model, mydata, threshold=1)
 pamr.plotcen(model, mydata, threshold=2.5)
-a=pamr.listgenes(model,mydata,threshold=2.5)
-cat( paste( colnames(data)[as.numeric(a[,1])], collapse='\n' ) )
-cvmodel=pamr.cv(model,mydata)
-print(cvmodel)
-pamr.plotcv(cvmodel)
+
+
+a_model<-pamr.listgenes(model,mydata,threshold=2.5)
+a_model[,1] #Variablerna
+my_variables_index<-as.numeric(a_model[,1])
+my_variables<-colnames(data)[my_variables_index]
+cat(paste(my_variables, collapse="\n")) #Here they are listed in propar manner.
+length(my_variables)
+
+cv_model<-pamr.cv(model,mydata)
+print(cv_model)
+pamr.plotcv(cv_model)
+
+#Tror jag har allt jag behöver ovan, utan att påstå att jag fattat vad som händer.
+#koden stulen fr:
+#https://liuonline.sharepoint.com/sites/732A95/732A95-2016HT/CourseDocuments/lecture3b_bl2.pdfhttps://liuonline.sharepoint.com/sites/732A95/732A95-2016HT/CourseDocuments/lecture3b_bl2.pdf
+#slide 11
