21fe597795ead09ffdd4653dc3f67c098ca901f8
Anton Persson <antpe404@student.liu.se>
2016-12-13 17:29:09 +0100

    Maybe some more understanding

--- lab3bl2_732A95.R
+++ lab3bl2_732A95.R
@@ -3,6 +3,8 @@
 #Assignment 1
 data<-read.csv2("data/data.csv", header=T, sep=";")
 #Wide data.
+set.seed(12345)
+data<-data[sample(1:nrow(data)), ] #Shufflar raderna
 data$Conference<-as.factor(data$Conference)
 train<-data[1:45, ]
 test<-data[46:64, ]
@@ -28,10 +30,30 @@ cat(paste(my_variables, collapse="\n")) #Here they are listed in propar manner.
 length(my_variables)
 
 cv_model<-pamr.cv(model,mydata)
-print(cv_model)
+cv_model$error#.13 lägst, fr 0.7-1.8 och 2.6
+#which((cv_model$error)==min(cv_model$error))
+#which((cv_model$loglik)==max(cv_model$loglik))#30
+print(cv_model) #Här kan man se samma, 0.7-1.8 eller 2.6
 pamr.plotcv(cv_model)
 
 #Tror jag har allt jag behöver ovan, utan att påstå att jag fattat vad som händer.
+chosen_model<-pamr.train(mydata,threshold=2.6) #Väljer 2.6 för simplicity.
+pamr.plotcen(chosen_model, mydata, threshold=2.6)
+
+cv_vars<-pamr.listgenes(chosen_model,mydata,threshold=2.6)
+cv_vars[,1] #Variablerna
+my_variables_index2<-as.numeric(cv_vars[,1])
+my_variables2<-colnames(data)[my_variables_index2]
+cat(paste(my_variables2, collapse="\n")) #Here they are listed in propar manner.
+length(my_variables2)
+
+x_test<-t(test[,-which_col]) #Alla utom Conf
+y_test<-test[, which_col] #endast Conf
+#head(as.character( 1:nrow(x) ))
+my_testdata<-list(x=x_test,y=y_test,geneid=as.character(1:nrow(x)), genenames=rownames(x))
+
+
+table(preds=pamr.predict(chosen_model,my_testdata$x,threshold = 2.6),truth=my_testdata$y)
 #koden stulen fr:
 #https://liuonline.sharepoint.com/sites/732A95/732A95-2016HT/CourseDocuments/lecture3b_bl2.pdfhttps://liuonline.sharepoint.com/sites/732A95/732A95-2016HT/CourseDocuments/lecture3b_bl2.pdf
 #slide 11
