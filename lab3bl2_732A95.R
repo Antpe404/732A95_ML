bd477a844471917fc1a93d2a55844ec25558f155
Anton Persson <antpe404@student.liu.se>
2016-12-14 15:37:52 +0100

    1.2

--- lab3bl2_732A95.R
+++ lab3bl2_732A95.R
@@ -51,28 +51,31 @@ y_test<-test[, which_col] #endast Conf
 #head(as.character( 1:nrow(x) ))
 my_testdata<-list(x=x_test,y=y_test,geneid=as.character(1:nrow(x)), genenames=rownames(x))
 
-table(preds=pamr.predict(chosen_model,my_testdata$x,threshold = 2.6),truth=my_testdata$y)
+conf_mat_NSC<-table(preds=pamr.predict(chosen_model,my_testdata$x,threshold = 2.6),truth=my_testdata$y)
+conf_mat_NSC
 #Tror jag har allt jag behöver ovan, utan att påstå att jag fattat vad som händer.
-
+number_of_features_NSC<-length(my_variables2)
+test_error_rate_NSC<-(conf_mat_NSC[1,2]+conf_mat_NSC[2,1])/sum(conf_mat_NSC)
+test_error_rate_NSC
 #koden stulen fr:
 #https://liuonline.sharepoint.com/sites/732A95/732A95-2016HT/CourseDocuments/lecture3b_bl2.pdfhttps://liuonline.sharepoint.com/sites/732A95/732A95-2016HT/CourseDocuments/lecture3b_bl2.pdf
 #slide 11
 
 
 #1.2
-#a
+#1.2a
 #	Elastic net, binomial response and alpha=0.5, penalty selected by cross-validation
 
 library(glmnet)  
-library(reshape2)   
 
 train_x<-as.matrix(train[,-which_col])
 train_y<-as.factor(train[,which_col])
 test_x<-as.matrix(test[, -which_col])
 test_y<-as.factor(test[,which_col])
-glmnet(x=train_x, y=train_y, alpha=.5, family="binomial")
 
+#glmnet(x=train_x, y=train_y, alpha=.5, family="binomial")
 #cv.glmnet verkar vara crossvalidated samma.
+set.seed(12345)
 elastic<-cv.glmnet(x=train_x, y=train_y, alpha=.5, family="binomial")
 plot(elastic)
 
@@ -84,12 +87,46 @@ elastic_coeff<-elastic_coe[,1]
 elastic_coef<-elastic_coeff[elastic_coeff!=0] #Tar ut de som inte är 0.
 
 elastic_coefficients<-as.data.frame(cbind(coef=as.numeric(elastic_coef), variable=names(elastic_coef)))
-#10 variabler med coef skild fr 0.
-
+#12 variabler med coef skild fr 0.
 fitted<-predict(elastic, newx = test_x, s = elastic$lambda.1se, type="class") #default s.
+
 fitted
 length(fitted)
 fitted<-as.numeric(fitted)
-confusion_matrix<-table(preds=fitted, truth=test_y)
-error_rate_elastic<-(confusion_matrix[1,2]+confusion_matrix[2,1])/sum(confusion_matrix)
 
+number_of_features_elastic<-nrow(elastic_coefficients)-1
+conf_mat_elastic<-table(preds=fitted, truth=test_y)
+test_error_rate_elastic<-(conf_mat_elastic[1,2]+conf_mat_elastic[2,1])/sum(conf_mat_elastic)
+
+fitted
+test_y
+
+#1.2b
+library(kernlab)
+set.seed(12345)
+my_svm<-ksvm(x=train_x, y=train_y, kernel="vanilladot", scale=FALSE, type="C-svc")
+my_svm #training error 0.02222, 44 suppoert vectors
+my_svm@coef #the corresponding coefficients times the training labels.
+length(my_svm@coef[[1]])
+my_svm@error
+my_svm@nSV #The number of support vectors. Think this is the optimal size
+
+fitted_svm<-predict(my_svm, test_x)
+conf_mat_svm<-table(preds=fitted_svm, truth=test_y)
+conf_mat_svm
+
+test_error_rate_svm<-(conf_mat_svm[1,2]+conf_mat_svm[2,1])/sum(conf_mat_svm)
+
+number_of_features_svm<-my_svm@nSV
+
+#sum(predict(my_svm, train_x)==train_y) 44/45 rätt i träning->0.02222
+
+#########################Comparison, last thing in ass 1.2###############################
+comparison_table<-data.frame(cbind(rbind(number_of_features_NSC, test_error_rate_NSC), rbind(number_of_features_elastic, test_error_rate_elastic),
+rbind(number_of_features_svm, test_error_rate_svm)))
+
+colnames(comparison_table)<-c("NSC", "Elastic", "SVM")
+rownames(comparison_table)<-c("# of features", "test_error_rate")
+comparison_table[1,]<-as.integer(comparison_table[1,])
+comparison_table
+#---------------------------------------------------------------------------------------#
