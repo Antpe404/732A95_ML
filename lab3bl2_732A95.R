622b5da7cc8e366309572c85880f45d1a5f0c06e
Anton Persson <antpe404@student.liu.se>
2016-12-15 16:02:06 +0100

    Prolly wrong bout row 243 step4[m]

--- lab3bl2_732A95.R
+++ lab3bl2_732A95.R
@@ -217,17 +217,35 @@ ind <- sample(1:nrow(spam))
 spam <- spam[ind,c(1:48,58)] #Shufflar och ta bort 
 h <- 1
 b<-0
-beta <- c(0, -.05)
-beta1<-0
-beta2<--.05
-  M <- c(500, 20)
-  M1<-500
-  M2<-20
+#beta <- c(0, -.05)
+beta<-(-.05)
+#beta2<--.05
+ # M <- c(500, 20)
+  M<-500
+  #M2<-20
   N <- 500 # number of training points
 
 gaussian_k <- function(x, h) { # Gaussian kernel
 ans<-exp(-(x**2)/(h**2))
 return(ans)
+}
+
+step8_func<-function(sv){
+  #last_part<-vector(length=length(sv))
+  res<-vector(length=length(sv))
+  index<-1
+  for(m in sv){
+    #m<-234
+    rad_m<-spam[m, -49]
+    t_m<-spam[m, 49]
+    dis<-as.matrix(dist(rbind(rad_m,rad_m))) #sista delen av step 8
+    last_part<-t_m*gaussian_k(x=dis[-1,1], h=h) #sista delen av step8, med dis mellan samma vector
+    yxm<-step4[m]#Motsvarar y(x_m) in step8, calculated in last loop
+    res[index]<-t_m*(yxm-last_part)
+    index<-index+1
+  }
+  #return(res)
+  return(which.max(res)) #tar ut den minst viktiga, i.e. max.
 }
 
 SVM <- function(sv,i) { # SVM on point i with support vectors sv
@@ -238,20 +256,26 @@ SVM <- function(sv,i) { # SVM on point i with support vectors sv
   # You can use dist() to compute the Euclidean distance
 }
 
-errors <- 1
-errorrate <- vector(length = N)
-errorrate[1] <- 1
-sv <- c(1)
-step4<-vector(length=N-1)
+
 
 #m_rad<-spam[sv, -49] #x_m
 #m_response<-spam[sv, 49] #t_m
-x_m<-spam[1, -49] #x_m
-t_m<-spam[1, 49] #t_m
+#x_m<-spam[1, -49] #x_m
+#t_m<-spam[1, 49] #t_m
 
 #x_m<-spam[sv, -49] #x_m
 #t_m<-spam[sv, 49] #t_m
+
+errors <- 1
+errorrate <- vector(length = N)
+errorrate[1] <- 1
+
+sv <- c(1)
+step4<-vector(length=N-1)
+
 for(i in 2:N) {
+  #sv<-1:2
+  #i<-1
   x_i<-spam[i, -49] #step3
   t_i<-spam[i, 49] #step3
   x_m<-spam[sv, -49] #x_m
@@ -262,12 +286,20 @@ for(i in 2:N) {
   yx_i<-sum(t_m*gaussian_k(x=distance[-1,1], h=h)+b) #step4
   if (t_i*yx_i<=beta){ #step5
     sv<- c(sv, i) #step6
+    errors<-errors+1
     #ignore step7
-    if (length(sv)>96){ #step 8
+    if (length(sv)>M){ #step 8
       sv <- sv[-step8_func(sv=sv)]
     }
   }
+  errorrate[i] <- errors / i
 }
+
+plot(errorrate)
+############errorrate är alltså andelen rader som lägger i sv. Mao är den 1 första iterationen,
+#eftersom rad två är med i sv (med setseed(12345)) är den efter 2 iteration fortf 1,
+#den tredje är inte med (se sv) ergo är raten då .667. 
+#Den fjärde är med ergo 3/4->.75 efter den it.
 
 sum(step4)
 plot(errorrate[seq(from=1, to=N, by=10)], ylim=c(0.2,0.4), type="o")
@@ -313,11 +345,11 @@ step8_func<-function(sv){
     res[index]<-t_m*(yxm-last_part)
     index<-index+1
   }
-  
+  #return(res)
   return(which.max(res))
 }
 
-step8_func(sv=sv)
+max(step8_func(sv=sv))
 
 
 
