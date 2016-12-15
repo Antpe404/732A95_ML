966ba65c054918fb752ff8d0c98305c50b7cd5de
Anton Persson <antpe404@student.liu.se>
2016-12-15 11:44:51 +0100

    Kaempar paa med ass2

--- lab3bl2_732A95.R
+++ lab3bl2_732A95.R
@@ -207,3 +207,68 @@ rownames(pvalues)<-colnames(data)[1:(ncol(data)-1)]
 
 pvalues[,1]<-sort(pvalues[,1])
 #---------------------------------------------------------
+
+###########################Assignment 2###############################################
+spam<-read.csv2("data/spambase_lab3bl2.csv", sep=";", header=T)
+spam$Spam[spam$Spam==0]<--1 #Convertar för att underlätta SVM:s.
+
+set.seed(1234567890)
+ind <- sample(1:nrow(spam))
+spam <- spam[ind,c(1:48,58)] #Shufflar och ta bort 
+h <- 1
+b<-0
+beta <- c(0, -.05)
+  M <- c(500, 20)
+  M1<-500
+  M2<-20
+  N <- 500 # number of training points
+
+gaussian_k <- function(x, h) { # Gaussian kernel
+ans<-exp(-(x**2)/(h**2))
+return(ans)
+}
+
+SVM <- function(sv,i) { # SVM on point i with support vectors sv
+  # Your code here
+  # Note that the labels in spambase.csv are 0/1 and SVMs need -1/+1. Then, use 2*label-1 # to convert from 0/1 to -1/+1
+  # Do not include the labels when computing the Euclidean distance between the point i 
+  # and each of the support vectors. This is the distance to use in the kernel function
+  # You can use dist() to compute the Euclidean distance
+}
+
+errors <- 1
+errorrate <- vector(length = N)
+errorrate[1] <- 1
+sv <- c(1)
+step4<-vector(length=N-1)
+
+#m_rad<-spam[sv, -49] #x_m
+#m_response<-spam[sv, 49] #t_m
+x_m<-spam[sv, -49] #x_m
+t_m<-spam[sv, 49] #t_m
+for(i in 2:N) {
+  x_i<-spam[i, -49]
+  t_i<-spam[i, 49]
+  distance<-dist(rbind(x_i, x_m), method="euclidean")
+  step4[i-1]<-sum(t_m*gaussian_k(x=distance, h=h)+b)
+  #ans<-sum(step4)
+  #anss<-sum(t_m*gaussian_k(x=distance, h=h)+b)
+}
+sum(step4)
+plot(errorrate[seq(from=1, to=N, by=10)], ylim=c(0.2,0.4), type="o")
+length(sv)
+errorrate[N]
+
+#Test nedan.
+#step3
+x_i<-spam[3, -49] #detta är ex på x_i
+t_i<-spam[3, 49] #detta är ex på t_i
+
+m_rad<-spam[sv, -49] #x_m
+m_response<-spam[sv, 49] #t_m
+#step4
+x_i
+m_rad
+test<-rbind(x_i, m_rad)
+test_dist<-as.matrix(dist(test, method="euclidean"))
+sum(m_response*gaussian_k(x=test_dist[-1,1], h=h)+b) #step4 slide, y(x_i)
