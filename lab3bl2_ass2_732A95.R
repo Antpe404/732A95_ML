a57417118b6056a8a5eb5d51c134fdb1afabc2e1
Anton Persson <antpe404@student.liu.se>
2016-12-16 11:54:16 +0100

    Think it's working

--- lab3bl2_ass2_732A95.R
+++ lab3bl2_ass2_732A95.R
@@ -4,40 +4,46 @@ spam$Spam[spam$Spam==0]<--1 #Convertar för att underlätta SVM:s.
 set.seed(1234567890)
 ind <- sample(1:nrow(spam))
 spam <- spam[ind,c(1:48,58)] #Shufflar och ta bort 
-h <- 1
-H<-1
-b<-0
+#h <- 1
+#H<-1
+#b<-0
 #beta <- c(0, -.05)
-beta<-(-.05)
-beta<-0
+#beta<-(-.05)
+#beta<-0
 #beta2<--.05
 # M <- c(500, 20)
-M<-20
+#M<-20
 #M2<-20
-N <- 500 # number of training points
+#N <- 500 # number of training points
 
 gaussian_k <- function(x, h) { # Gaussian kernel
-  ans<-exp(-(x**2)/(h**2))
+  ans<-exp(-(x**2)/(2*h**2))
   return(ans)
 }
 
-step8_func<-function(sv){
+step8_func<-function(sv, h){
+  b<-0
   #last_part<-vector(length=length(sv))
   res<-vector(length=length(sv))
   index<-1
   for(m in sv){
     #m<-234
-    rad_m<-spam[m, -49]
-    t_m<-spam[m, 49]
-    dis<-as.matrix(dist(rbind(rad_m,rad_m))) #sista delen av step 8
-    last_part<-t_m*gaussian_k(x=dis[-1,1], h=H) #sista delen av step8, med dis mellan samma vector
+    x_m<-spam[m, -49] #step3
+    t_m<-spam[m, 49] #step3
+    dis<-as.matrix(dist(rbind(x_m,x_m))) #sista delen av step 8
+    last_part<-sum(t_m*gaussian_k(x=dis[-1,1], h=h)) #sista delen av step8, med dis mellan samma vector
     #Nedan ist för step4[m]
-    x_i<-spam[i, -49] #step3
-    t_i<-spam[i, 49] #step3
-    x_m<-spam[m, -49] #x_m
-    t_m<-spam[m, 49]
-    distance<-as.matrix(dist(rbind(x_i, x_m), method="euclidean"))
-    yxm<-sum(t_m*gaussian_k(x=distance[-1,1], h=H)+b)
+    x_sv<-spam[sv, -49] #x_m
+    t_sv<-spam[sv, 49]
+    distance<-as.matrix(dist(rbind(x_m, x_sv), method="euclidean"))
+    #step4[i-1]<-sum(t_m*gaussian_k(x=distance[-1,1], h=H)+b)
+    yxm<-sum(t_sv*gaussian_k(x=distance[-1,1], h=h)+b) #step4
+    
+    
+    #x_m<-spam[m, -49] #x_m
+    #t_m<-spam[m, 49]
+    #distance<-as.matrix(dist(rbind(x_m, x_m), method="euclidean"))
+    #yxm<-sum(t_m*gaussian_k(x=distance[-1,1], h=h)+b)
     #
     #yxm<-step4[m]#Motsvarar y(x_m) in step8, calculated in last loop
     res[index]<-t_m*(yxm-last_part)
@@ -52,7 +58,7 @@ SVM<-function(sv=c(1), H=1, b=0, N=500, M, beta){
   errors <- 1
   errorrate <- vector(length = N)
   errorrate[1] <- 1
-  step4<-vector(length=N-1)
+  #step4<-vector(length=N-1)
   
   for(i in 2:N) {
     #sv<-1:2
@@ -62,20 +68,23 @@ SVM<-function(sv=c(1), H=1, b=0, N=500, M, beta){
     x_m<-spam[sv, -49] #x_m
     t_m<-spam[sv, 49]
     distance<-as.matrix(dist(rbind(x_i, x_m), method="euclidean"))
-    step4[i-1]<-sum(t_m*gaussian_k(x=distance[-1,1], h=H)+b)
-    
+    #step4[i-1]<-sum(t_m*gaussian_k(x=distance[-1,1], h=H)+b)
     yx_i<-sum(t_m*gaussian_k(x=distance[-1,1], h=H)+b) #step4
-    if (t_i*yx_i<=beta){ #step5
+   
+     if (t_i*yx_i<=beta){ #step5
       sv<- c(sv, i) #step6
-      errors<-errors+1
+      
       #ignore step7
       if (length(sv)>M){ #step 8
-        sv <- sv[-step8_func(sv=sv)]
+        sv <- sv[-step8_func(sv=sv, h=H)]
       }
+     }
+    if (t_i*yx_i<=0){
+    errors<-errors+1
     }
     errorrate[i] <- errors / i
   }
-  plot(errorrate, main=paste("Errorate over iterations, M=", M, ", beta=", beta,"."))
+  plot(errorrate, main=paste("Errorrate over iterations, M=", M, ", beta=", beta,"."))
 }
 
 SVM(M=500, beta=0)
@@ -113,7 +122,7 @@ for(i in 2:N) {
     errors<-errors+1
     #ignore step7
     if (length(sv)>M){ #step 8
-      sv <- sv[-step8_func(sv=sv)]
+      sv <- sv[-step8_func(sv=sv, h=H)]
     }
   }
   
